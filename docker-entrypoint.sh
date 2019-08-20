ce913d0acc27679e45ceb9a5da71174f1e6bd851
Yee-Ting Li <yee379@gmail.com>
2019-08-20 12:54:50 -0700

    allow munge.key to be copied in

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# setup munge
+if [ -e /mnt/munge/munge.key ]; then
+  cp /mnt/munge/munge.key /etc/munge/munge.key
+  chown munge:munge /etc/munge/munge.key
+  chmod 400 /etc/munge/munge.key
+fi
+
+# setup slurm
 chown slurm:slurm /var/spool/slurmd /var/run/slurmd /var/lib/slurmd /var/log/slurm
 
 # pick up relevant supervisord conf
 SUPERVISORD_CONFIG=${SUPERVISORD_CONFIG:-/etc/slurmctld-supervisord.conf}
-
 echo "Starting with $SUPERVISORD_CONFIG..."
-
-hostname -s
-
 exec /usr/bin/supervisord --configuration $SUPERVISORD_CONFIG
 
